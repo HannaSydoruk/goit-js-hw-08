5a5c25ea9b3e3285bd5cf935b071a0845887c24d
HannaSydoruk <hanna.sydoruk@gmail.com>
2023-10-18 11:48:30 +0300

    Tesk3, Task2 fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('.feedback-form input')
+const textareaEl = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('input', throttle(onTextareaEdit, 500));
+form.addEventListener('submit', onFormSubmit);
+
+fillForm();
+
+function onTextareaEdit() {
+    const email = emailEl.value;
+    const message = textareaEl.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    console.log(localStorage.getItem('feedback-form-state'));
+    localStorage.removeItem('feedback-form-state');
+
+}
+
+function fillForm() {
+    const savedInfo = localStorage.getItem('feedback-form-state');
+    const parsedInfo = JSON.parse(savedInfo);
+    if (parsedInfo) {
+        emailEl.value = parsedInfo.email;
+        textareaEl.value = parsedInfo.message;
+    }
+}
